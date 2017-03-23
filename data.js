6bce61a4a04881963525ac5406f9202f4f7e2e95
Josh <spongejr@gmail.com>
2017-03-23 02:33:41 -0400

    starter list

---
+++ data.js
@@ -0,0 +1,44 @@
+A video where Sal says “...and we’re done!”
+A video only available in low resolution (not HD / 480+)
+A video that has an error in the captions
+A video with both Sal and Brit Cruise
+A video where you learned something brand new
+A video with 0 Questions
+A video about something very recent/new
+A video about something very old
+A programming talk-through where Hopper is mentioned
+A Community Explanation article or activity
+Something about science
+Something technology
+Something about engineering
+Something about art
+Something about math
+A video that mentions food
+A video that mentions an animal
+A video that mentions a game or sport
+A video with somebody famous
+An article with interactive features embedded
+A video where something hot is mentioned
+A video where something cold is mentioned
+An article about Khan Academy
+A video about Khan Academy
+An exercise you haven’t done before
+An exercise you have done before
+A piece of content (article, video, exercise, etc.) created from a user contributor
+A video where someone says a word or phrase in a language other than English
+A video or article about something from your part of the world
+A video or article about something from another part of the world
+A video or article about something that’s out of this world
+A page on khanacademy.org that has something that you created
+A page on khanacademy.org that has something that someone you know created
+A video or article that mentions energy points or badges
+Something funny
+A non-math exercise
+A video with a king
+A video with music
+A game
+A broken link
+A video you’ve already watched
+A video you haven’t watched
+A video that shows an older version of the Khan Academy site
+Something you’ve shared with someone else
